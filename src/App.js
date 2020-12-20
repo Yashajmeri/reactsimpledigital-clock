1dc5b8c170ef4663bfc9c1f20cd0eeef4a5a51eb
Yash Ajmeri <ajmeriyash1312@gmail.com>
2020-12-20 18:58:11 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+   setInterval(updateTime,1000);
+  const now = new Date().toLocaleTimeString();
+
+  const[laps,setlaps] = useState('');
+  
+  const [time,setTime] = useState(now);
+  function updateTime(){
+    const newTime = new Date().toLocaleTimeString();
+      setTime(newTime);
+     
+  }
+  function handleClick(){
+    const newTime = new Date().toLocaleTimeString();
+    setlaps(newTime);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+         <div className="container">
+        <h1>{time}</h1>
+        <p>{laps}</p>
+        <button onClick={handleClick}>PUSH TIME</button>
+
+         </div>
   );
 }
 
